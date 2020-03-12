7e6071701ffdfc7cfebb086a0ed121b1a9ddc86d
Sofiia Rodina <rodinas@ceos.imag.fr>
2020-03-12 15:48:10 +0100

    add notebook

--- utils/model_input_generator.py
+++ utils/model_input_generator.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import random
 
 
 class ModelInputGenerator:
     def __init__(self, docs, queries, qrels):
+        random.seed(0)
         self.docs = docs
         self.queries = queries
         self.qrels = qrels
@@ -14,17 +16,24 @@ class ModelInputGenerator:
         self.df_docs = pd.read_csv(self.docs, na_filter=False)
         self.df_queries = pd.read_csv(self.queries, na_filter=False)
         self.df_qrels = pd.read_csv(self.qrels, sep="\t", na_filter=False, header=None)
+        self.docs_len = self.df_docs.shape[0]
         print("Finished.")
 
+        
+    def __randidx(self, a, b, val):
+        while True:
+            x = random.randint(a, b)
+            if x != val:
+                return x
     """
-    Returns the batch of 4-values: 'query doc_1 doc_2 relevance_label'
+    Returns the batch of 3-values: 'query doc_1(relevant) doc_2(irrelevant)'
     """
 
     def generate_batch(self, size=256):
         batch = []
         for i in range(self.offset, self.offset + size):
             sample = []
-            qrel = self.df_qrels.loc[i]  # id_query, 0, id_doc, relevance
+            qrel = self.df_qrels.loc[i]  # id_query, 0, id_doc
             sample.append(
                 self.df_queries.loc[self.df_queries["id_left"] == qrel[0]][
                     "text_left"
@@ -38,12 +47,12 @@ class ModelInputGenerator:
             )
 
             sample.append(
-                self.df_docs.loc[self.df_docs["id_right"] != qrel[2]][
-                    "text_right"
-                ].values[0]
+                self.df_docs.loc[self.__randidx(0, self.docs_len, qrel[2])]["text_right"]
             )
-            sample.append("1")
 
             batch.append(sample)
         self.offset += size
         return batch
+    
+    def reset(self, val=0):
+        self.offset = val
