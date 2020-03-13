699b897b897114337cf1f24e8f9931479a19efbf
rodinasophie <rodinasophie@gmail.com>
2020-03-13 17:49:58 +0100

    v1

---
+++ snrm/representations/autoencoder.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+"""
+Autoencoder neural network to learn document representation.
+
+"""
+
+
+class Autoencoder(nn.Module):
+    def __init__(self, layer_size, dropout_prob=0.6):
+        super().__init__()
+        self.layer_size = layer_size
+        self.fc = nn.ModuleList([])
+        for i in range(len(layer_size) - 1):
+            self.fc.append(
+                nn.Conv2d(layer_size[i], layer_size[i + 1], (1, 5 if i == 0 else 1))
+            )
+        self.dropout = nn.Dropout(p=dropout_prob, inplace=False)
+
+    def forward(self, x):
+        for i in range(len(self.fc)):
+            x = self.dropout(F.relu(self.fc[i](x)))
+        x = torch.mean(x, 3, keepdim=True)
+        return x
+
--- utils/model_input_generator.py
+++ utils/model_input_generator.py
@@ -9,6 +9,7 @@ class ModelInputGenerator:
         self.queries = queries
         self.qrels = qrels
         self.offset = 0
+        self.doc_offset = 0
         self.__preprocess_data()
 
     def __preprocess_data(self):
@@ -17,14 +18,15 @@ class ModelInputGenerator:
         self.df_queries = pd.read_csv(self.queries, na_filter=False)
         self.df_qrels = pd.read_csv(self.qrels, sep="\t", na_filter=False, header=None)
         self.docs_len = self.df_docs.shape[0]
+        self.qrel_len = self.df_qrels.shape[0]
         print("Finished.")
 
-        
     def __randidx(self, a, b, val):
         while True:
             x = random.randint(a, b)
             if x != val:
                 return x
+
     """
     Returns the batch of 3-values: 'query doc_1(relevant) doc_2(irrelevant)'
     """
@@ -47,12 +49,32 @@ class ModelInputGenerator:
             )
 
             sample.append(
-                self.df_docs.loc[self.__randidx(0, self.docs_len, qrel[2])]["text_right"]
+                self.df_docs.loc[self.__randidx(0, self.docs_len, qrel[2])][
+                    "text_right"
+                ]
             )
 
             batch.append(sample)
         self.offset += size
         return batch
-    
+
+    def generate_docs(self, size=256):
+        batch = []
+        for i in range(self.doc_offset, self.doc_offset + size):
+            batch.append(
+                (self.df_docs.loc[i]["id_right"], self.df_docs.loc[i]["text_right"])
+            )
+        self.offset += size
+        return batch
+
+    def reset_doc(self, val=0):
+        self.doc_offset = val
+
     def reset(self, val=0):
         self.offset = val
+
+    def qrel_length(self):
+        return self.qrel_len
+
+    def docs_length(self):
+        return self.docs_len
