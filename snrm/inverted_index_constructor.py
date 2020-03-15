2c5db50024413b2b947e81f9f65e13c2e0ba045b
rodinasophie <rodinasophie@gmail.com>
2020-03-15 14:58:48 +0100

    update

--- snrm/inverted_index_constructor.py
+++ snrm/inverted_index_constructor.py
@@ -3,14 +3,14 @@ class InvertedIndexConstructor:
         self.index = dict()
         self.out_file = out_file
 
-    def construct(self, batch):
+    def construct(self, doc_ids, batch):
         repres_len = batch.shape[1]
         for i in range(batch.shape[0]):
             for j in range(repres_len):
-                if batch[i][1][j] > 0.0:
+                if batch[i][j] > 0.0:
                     if j not in self.index:
                         self.index[j] = []
-                    self.index[j].append((batch[i][0], batch[i][1][j]))
+                    self.index[j].append((doc_ids[i], batch[i][j]))
 
     def get_index(self):
         return self.index
